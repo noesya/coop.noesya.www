7ca35107406827e1046fc4456d3c301b94aac45c
alexisben <alexiben7@gmail.com>
2021-10-02 22:03:37 +0200

    Add hidden scrollable menu

---
+++ _includes/js/menu.js
@@ -0,0 +1,42 @@
+window.menu = {
+    y: 0,
+    isOpened: false,
+    init: function () {
+        'use strict';
+        this.dom = document.querySelector('#header');
+        this.button = this.dom.querySelector('button');
+
+        this.bind();
+    },
+    bind: function () {
+        'use strict';
+
+        this.button.addEventListener('click', function () {
+            document.documentElement.classList.toggle('is-menu-opened');
+            this.isOpened = !this.isOpened;
+        }.bind(this));
+
+        window.addEventListener('scroll', this.scroll.bind(this));
+    },
+    scroll: function () {
+        'use strict';
+        var y = window.scrollY;
+
+        if (this.isOpened) {
+            return;
+        }
+
+        if (this.y > y || y < 50) {
+            this.dom.classList.remove('is-hidden');
+        } else {
+            this.dom.classList.add('is-hidden');
+        }
+
+        this.y = y;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+    window.menu.init();
+});
