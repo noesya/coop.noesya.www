5c5baf417a3a99229f38a8af951e5da63af174a2
alexisben <alexiben7@gmail.com>
2021-10-04 09:44:20 +0200

    Add minify js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,5 @@
 const gulp = require('gulp');
-const htmlmin = require('gulp-htmlmin');
-const cleanCSS = require('gulp-clean-css');
-const purgecss = require('gulp-purgecss');
-const prefix = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
-
-function html () {
-  return gulp.src('_site/**/*.html')
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('_site'));
-};
 
 function js () {
   return gulp.src('_site/**/*.js')
@@ -17,15 +7,4 @@ function js () {
     .pipe(gulp.dest('_site'));
 };
 
-function css () {
-  return gulp.src('_site/**/*.css')
-    .pipe(purgecss({
-      content: ['_site/**/*.html']
-    }))
-    .pipe(prefix())
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('_site'));
-};
-
 exports.default = gulp.parallel(js);
-// exports.default = gulp.parallel(css, js, html);
